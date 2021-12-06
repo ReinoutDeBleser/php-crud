02bb28330cf75ee98e293002a2d2afe9981b098a
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-06 18:34:36 +0100

    Update Teacher.php
    Created methods to get, save, update and delete information from the database

--- Model/Teacher.php
+++ Model/Teacher.php
@@ -1,70 +1,100 @@
 <?php
+    declare(strict_types=1);
 
-require '../class/Database.php';
+    require ('../../class/Database.php');
 
-class Teacher
-{
-    private $firstName;
-    private $lasttName;
-    private $email;
-    private $phone;
-    private $studet;
-    private $classroom;
+// teacher = Teacher.show(1)
 
-    public function __construct(){
-    }
+    class Teacher
+    {
+        private $firstName;
+        private $lastName;
+        private $email;
+        private $phone;
+        private $studet;
+        private $classroom;
 
-    public function read($id){
-        $row = Database::query('SELECT * FROM' . $this->table . 'WHERE id = '. $id);
-        $this->firstName = $row['firstName'];
-        $this->lastName = $row['lastName'];
-        $this->email = $row['email'];
-        $this->phone = $row['phone'];
-        $this->classroom = $row['classroom'];
-    }
-
-    public function create($firstName, $lasttName, $email, $phone, $classroom){
-        $query = "INSERT INTO teacher (firstName, lasttName, email, phone, classroom)
-        VALUES ('$firstName', '$lasttName', '$email', '$phone', '$classroom')";
-
-        $insert = Databese::query($query);
-
-        if ($insert) {
-            echo "<p> Your registration is complete! </p><br>";
-        } else {
-            echo "<p> Unable to register, try again!</p>";
-        }
-    }
-
-    
-    public function update($id, $firstName, $lasttName, $email, $phone, $classroom){
-        $query = 'UPDATE teacher
-        SET firstName =' . $firstName. ', lasttName =' . $lasttName . ', email = ' . $email . ', phone = ' . $phone . ', 
-        classroom = ' . $classroom .
-        'WHERE id = '.$id;
-
-        $update = Database::query($query);
-
-        if ($update) {
-            echo "<p> Your update is complete! </p><br>";
-        } else {
-            echo "<p> Unable to update, try again!</p>";
+        public function __construct(){
         }
 
-    }
-
-    public function delete($id){
-        $query = 'DELETE FROM teacher WHERE id =' . $id;
-        $delete = Database::query($query);
-
-        if ($delete) {
-            echo "<p> It was deleted! </p><br>";
-        } else {
-            echo "<p> It is not working, try again!</p>";
+        public function readOneTeacher($id){
+            $row = Database::query('SELECT * FROM teacher' . 'WHERE id = '. $id);
+            $this->firstName = $row['firstName'];
+            $this->lastName = $row['lastName'];
+            $this->email = $row['email'];
+            $this->phone = $row['phone'];
+            $this->classroom = $row['classroom'];
         }
+
+        public function readAllTeacher(){
+            $row = $row = Database::query('SELECT * FROM teacher');
+            $query = Database::query($row);
+
+            while ($query = Database::query($row)){
+                $teachers[] = array("id" => $id, "firstName" => $row['firstName'], "lastName" => $row['lastName'], "email" => $row['email'], 
+                "phone" => $row['phone'], "class" => $row['classroom']);
+
+            }
+            if(!isset($teachers)){
+                return null;
+        }else{
+                return $teachers;
+        }
+        }
+
+        // public static function show($id) {
+        //     $row = Database::query('SELECT * FROM' . $this->table . 'WHERE id = '. $id);
+        //     return new Teacher($row['firstName'], )
+        // }
+
+        // public static function show($id) {
+        //     $rows = Database::query('SELECT * FROM' . $this->table);
+        //     return new Teacher($row['firstName'], )
+        // }
+
+        public function create($firstName, $lastName, $email, $phone, $classroom){
+            $query = "INSERT INTO teacher (firstName, lastName, email, phone, classroom)
+            VALUES ('$firstName', '$lastName', '$email', '$phone', '$classroom')";
+
+            $insert = Database::query($query);
+            var_dump($insert);
+
+            if ($insert) {
+                echo "<p> Your registration is complete! </p><br>";
+            } else {
+                echo "<p> Unable to register, try again!</p>";
+            }
+        }
+
+        
+        public function update($id, $firstName, $lastName, $email, $phone, $classroom){
+            $query = 'UPDATE teacher
+            SET firstName =' . $firstName. ', lastName =' . $lastName . ', email = ' . $email . ', phone = ' . $phone . ', 
+            classroom = ' . $classroom .
+            'WHERE id = '.$id;
+
+            $update = Database::query($query);
+
+            if ($update) {
+                echo "<p> Your update is complete! </p><br>";
+            } else {
+                echo "<p> Unable to update, try again!</p>";
+            }
+
+        }
+
+        public function delete($id){
+            $query = 'DELETE FROM teacher WHERE id =' . $id;
+            $delete = Database::query($query);
+
+            if ($delete) {
+                echo "<p> It was deleted! </p><br>";
+            } else {
+                echo "<p> It is not working, try again!</p>";
+            }
+        }
+
+
+
+
     }
-
-
-
-
-}
