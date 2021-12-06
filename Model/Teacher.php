17e991799db19d8c0ee263313562a88ed28a503c
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-06 12:09:33 +0100

    Update Teacher.php
    Created the methods:
    -> read to post the data in the browser;
    -> create to add a new teacher;
    -> update to update a teacher's information;
    -> delete to delete a teacher from the database

--- Model/Teacher.php
+++ Model/Teacher.php
@@ -1,6 +1,70 @@
 <?php
 
+require '../class/Database.php';
+
 class Teacher
 {
+    private $firstName;
+    private $lasttName;
+    private $email;
+    private $phone;
+    private $studet;
+    private $classroom;
+
+    public function __construct(){
+    }
+
+    public function read($id){
+        $row = Database::query('SELECT * FROM' . $this->table . 'WHERE id = '. $id);
+        $this->firstName = $row['firstName'];
+        $this->lastName = $row['lastName'];
+        $this->email = $row['email'];
+        $this->phone = $row['phone'];
+        $this->classroom = $row['classroom'];
+    }
+
+    public function create($firstName, $lasttName, $email, $phone, $classroom){
+        $query = "INSERT INTO teacher (firstName, lasttName, email, phone, classroom)
+        VALUES ('$firstName', '$lasttName', '$email', '$phone', '$classroom')";
+
+        $insert = Databese::query($query);
+
+        if ($insert) {
+            echo "<p> Your registration is complete! </p><br>";
+        } else {
+            echo "<p> Unable to register, try again!</p>";
+        }
+    }
+
+    
+    public function update($id, $firstName, $lasttName, $email, $phone, $classroom){
+        $query = 'UPDATE teacher
+        SET firstName =' . $firstName. ', lasttName =' . $lasttName . ', email = ' . $email . ', phone = ' . $phone . ', 
+        classroom = ' . $classroom .
+        'WHERE id = '.$id;
+
+        $update = Database::query($query);
+
+        if ($update) {
+            echo "<p> Your update is complete! </p><br>";
+        } else {
+            echo "<p> Unable to update, try again!</p>";
+        }
+
+    }
+
+    public function delete($id){
+        $query = 'DELETE FROM teacher WHERE id =' . $id;
+        $delete = Database::query($query);
+
+        if ($delete) {
+            echo "<p> It was deleted! </p><br>";
+        } else {
+            echo "<p> It is not working, try again!</p>";
+        }
+    }
+
+
+
 
 }
