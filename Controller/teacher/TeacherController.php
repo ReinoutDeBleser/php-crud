1461b2f093c62ad04976c92bf5669b30563a05f5
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-07 13:47:02 +0100

    Update TeacherController.php
    checked if the method delete from the Model is working and it is working.
    
    The method update from the Model is not working yet

--- Controller/teacher/TeacherController.php
+++ Controller/teacher/TeacherController.php
@@ -1,9 +1,26 @@
 <?php
+    declare(strict_types=1);
+    require ("../../Model/Teacher.php");
+// class TeacherController
+// {
+//     public function render(array $GET, array $POST)
+//     {
+//         require './View/teacher/page.php';
+//     }
+// }
 
-class TeacherController
-{
-    public function render(array $GET, array $POST)
-    {
-        require './View/teacher/page.php';
-    }
+$data = new Teacher();
+
+$teachers = $data->readAllTeacher();
+// var_dump($teachers);
+
+
+if(isset($_POST['delete'])){
+    $data->delete(1);
 }
+
+if(isset($_POST['update'])){
+    $data->update(12);
+}
+
+
