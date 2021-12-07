36637f13ce96a5c3cdc02eb63e0366f123907733
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-07 15:14:20 +0100

    Update CreateTeacherController.php
    moved all code from this file to the TeacherController.php

--- Controller/teacher/CreateTeacherController.php
+++ Controller/teacher/CreateTeacherController.php
@@ -5,66 +5,65 @@
 
 // class CreateTeacherController
 // {
-    $teacher = new Teacher();
+//     $teacher = new Teacher();
 
-    $check = true;
-    $firstName = $lastName = $email = $phone = $classroom = "";
-    $firstNameErr = $lastNameErr  = $emailErr  = $phoneErr  = $classroomErr  = "";
+//     $check = true;
+//     $firstName = $lastName = $email = $phone = $classroom = "";
+//     $firstNameErr = $lastNameErr  = $emailErr  = $phoneErr  = $classroomErr  = "";
 
-    echo $firstName;
+//     echo $firstName;
     
 
-    // public function render(array $GET, array $POST)
-    // {
-    //     // require '../../View/teacher/create.php';
-    //     die("test");
-    // }
+//     // public function render(array $GET, array $POST)
+//     // {
+//     //     // require '../../View/teacher/create.php';
+//     //     die("test");
+//     // }
     
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
-        if(empty($_POST['firstName'])){
-             $firstNameErr = "An name is required!";
-             $check = false;
-        }else{
-             $username = $_POST['firstName'];
-        }
+//     if($_SERVER["REQUEST_METHOD"] == "POST"){
+//           if(empty($_POST['firstName'])){
+//                $firstNameErr = "An name is required!";
+//                $check = false;
+//           }else{
+//                $username = $_POST['firstName'];
+//                echo $firstName;
+//           }
 
-        if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
-             $emailErr = "A valid email is required!";
-        }else{
-             $email = $_POST["email"];
-        }
+//           if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+//                $emailErr = "A valid email is required!";
+//           }else{
+//                $email = $_POST["email"];
+//           }
 
-        if(empty($_POST['lastName'])){
-             $lastNameErr = "A last name is required!";
-             $check = false;
-        }else{
-             $password1 = $_POST['lastName'];
-        }
+//           if(empty($_POST['lastName'])){
+//                $lastNameErr = "A last name is required!";
+//                $check = false;
+//           }
 
-        if(empty($_POST['phone'])){
-             $phoneErr = "A phone is required!";
-             $check = false;
-        }
+//           if(empty($_POST['phone'])){
+//                $phoneErr = "A phone is required!";
+//                $check = false;
+//           }
 
-        if(empty($_POST['classroom'])){
-            $classroomErr = "A classroom is required!";
-            $check = false;
-        }
-    }
+//           if(empty($_POST['classroom'])){
+//                $classroomErr = "A classroom is required!";
+//                $check = false;
+//           }
+//     }
 
-    if($check == true && $firstName == $_POST['firstName'] && $lastName == $_POST['lastName'] && 
-        $email == $_POST["email"] && $phone == $_POST['phone'] && $classroom == $_POST['classroom']){
-            if(isset($_POST['submit'])){
-                saveData($firstName, $lastName, $email, $phone, $classroom);
-            }
-    }else{
-        echo "nao deu certo";
-    }
+//     if($check == true && $firstName == $_POST['firstName'] && $lastName == $_POST['lastName'] && 
+//         $email == $_POST["email"] && $phone == $_POST['phone'] && $classroom == $_POST['classroom']){
+//           if(isset($_POST['submit'])){
+//                saveData($firstName, $lastName, $email, $phone, $classroom);
+//           }
+//      }else{
+//           echo "nao deu certo";
+//      }
 
-   function saveData(){
-    $teacher->create($firstName, $lastName, $email, $phone, $classroom);
-   }
+//    function saveData(){
+//      $teacher->create($firstName, $lastName, $email, $phone, $classroom);
+//    }
 // }
 
     
