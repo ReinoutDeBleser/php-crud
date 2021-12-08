0b3ba534183604211e6e5375c55f9ae32d71e10b
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-08 16:31:32 +0100

    Update StudentLoader.php
    changed $this->table for the name of the table

--- Model/StudentLoader.php
+++ Model/StudentLoader.php
@@ -21,6 +21,6 @@ class StudentLoader
 
 
     public function  getStudentByClassroom($classroom_id){
-        return Database::query('SELECT * FROM ' . $this->table . ' WHERE classroom_id = ' . $classroom_id);
+        return Database::query('SELECT * FROM student WHERE classroom_id = ' . $classroom_id);
     }
 }
