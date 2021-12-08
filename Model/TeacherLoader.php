b307b36e4945867e7400532758ac2fcc09d9780f
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-08 15:10:45 +0100

    Update TeacherLoader.php
    Now I get all teachers and only one teacher from the TeacherLoader class

--- Model/TeacherLoader.php
+++ Model/TeacherLoader.php
@@ -3,4 +3,16 @@
 class TeacherLoader
 {
 
+    public function readOneTeacher($id){
+
+        $row = Database::query('SELECT * FROM teacher WHERE id = '. $id);
+        return $row;
+    
+    }
+
+    public function readAllTeacher(){
+        $query = 'SELECT * FROM teacher';
+        $row = Database::query($query);
+        return $row;
+    }
 }
