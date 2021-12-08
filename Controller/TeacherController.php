fa207abda685ea07ef4411372cab163712f99320
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-08 11:18:17 +0100

    Update TeacherController.php
    Put all code of form validation in the function createTeacher and now and can show the errors messages on the page

--- Controller/TeacherController.php
+++ Controller/TeacherController.php
@@ -5,20 +5,18 @@
 
 class TeacherController
 {
+
     public function render(array $GET, array $POST)
     {
 
-        $data = new Teacher();
-
-        $teachers = $data->readAllTeacher();
+        $model = new Teacher();
+              
+        $teachers = $model->readAllTeacher();
 
         if(isset($_GET['view']) && $_GET['view'] == 'create') {
+            
             require './View/teacher/create.php';
         }
-
-        
-
-        
 
 
         if(isset($_POST['delete'])){
@@ -32,53 +30,55 @@ class TeacherController
         require 'View/teacher/index.php';
 
         // code to create a teacher
+    }
 
-        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['create'])){
-            $teacher = new Teacher();
+    public function createTeacher(){
+        $teacher = new Teacher();
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
             $check = true;
-            $firstname = $lastname = $email = $phone = $classroom = "";            
-    
-            var_dump($firstname, $lastname, $email, $phone, $classroom);
-    
+            $response =[];
+            $firstname = $_POST['firstname'];
+            $lastname = $_POST['lastname'];
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+            $classroom = $_POST['classroom']; 
+                    
             if(empty($_POST['firstname'])){
                 $errors['firstname'] = "An name is required!";
                 $check = false;
-            }else{
-                $firstname = $_POST['firstname'];
             }
             if(empty($_POST['lastname'])){
                 $errors['lastname'] = "A last name is required!";
                 $check = false;
-            }else{
-                $lastname = $_POST['lastname'];
             }
             
             if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
                 $errors['email'] = "A valid email is required!";
-            }else{
-                $email = $_POST["email"];
             }
     
             if(empty($_POST['phone'])){
                 $errors['phone'] = "A phone is required!";
                 $check = false;
-            }else{
-                $phone = $_POST['phone'];
             }
     
             if(empty($_POST['classroom'])){
                 $errors['classroom'] = "Which classroom doe this student belongs to?";
                 $check = false;
-            }else{
-                $classroom = $_POST['classroom'];
             }
     
-            if($check !== false){
+            if($check === true){
                 $teacher->create($firstname, $lastname, $email, $phone, $classroom);
-            }
-        }
+                $response['status'] = 'success';
 
+            }else{
+                $response['status'] = 'error';
+                $response['errors'] = $errors;
+            }
+           return $response;
+        }
     }
+    
 }
 
             
