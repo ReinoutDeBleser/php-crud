d3e3b431e217404f4c3b0ccb5303232ac1a64113
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-08 14:04:38 +0100

    Update single.php
    Now I can show only one teacher

--- View/teacher/single.php
+++ View/teacher/single.php
@@ -11,19 +11,19 @@
         <!-- Image for mobile view-->
         <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('../../assets/images/default-profile.jpg')"></div>
 
-        <h1 class="text-3xl font-light pt-8 lg:pt-0"><?php echo $student[0]['firstname']; ?> <?php echo $student[0]['lastname']; ?></h1>
+        <h1 class="text-3xl font-light pt-8 lg:pt-0"><?php echo $oneTeacher[0]['firstname']; ?> <?php echo $oneTeacher[0]['lastname']; ?></h1>
         <div class="mx-auto lg:mx-0 w-4/5 pt-3 mb-6 border-b-2 border-blue-500 opacity-25"></div>
         <p class="py-4 text-base flex items-center justify-center lg:justify-start">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 text-gray-600 pr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20" />
             </svg>
-            <?php echo $student[0]['email']; ?>
+            <?php echo $oneTeacher[0]['email']; ?>
         </p>
         <p class="py-4 text-base flex items-center justify-center lg:justify-start">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 text-gray-600 pr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
             </svg>
-            <?php echo $student[0]['phone']; ?>
+            <?php echo $oneTeacher[0]['phone']; ?>
         </p>
 
         <p class="py-4 text-base flex items-center justify-center lg:justify-start">
@@ -36,12 +36,12 @@
         </p>
 
         <div  class="pt-12 pb-8">
-            <a href="student.php?view=update&id=<?php echo $student[0]['id']; ?>">
+            <a href="teacher.php?view=update&id=<?php echo $oneTeacher[0]['id']; ?>">
             <button class="bg-yellow-600 hover:bg-yellow-800 border border-yellow-900 text-white font-bold py-2 px-8 mx-2 rounded-full">
                 Update
             </button>
             </a>
-            <a href="student.php?view=delete&id=<?php echo $student[0]['id']; ?>">
+            <a href="teacher.php?view=delete&id=<?php echo $oneTeacher[0]['id']; ?>">
             <button class="bg-red-700 hover:bg-red-800 border border-red-900 text-white font-bold py-2 px-8 mx-2 rounded-full">
                 Delete
             </button>
