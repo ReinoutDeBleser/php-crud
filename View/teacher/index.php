1889b01ca9a1d6181619a6b02a4ea50036e48768
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-09 16:11:18 +0100

    Now all links form the page are working

--- View/teacher/index.php
+++ View/teacher/index.php
@@ -62,9 +62,11 @@ Class
                     <?php echo $teacher['phone']; ?></div>
                 </td>
               <td class="px-6 py-4 whitespace-nowrap">
+              <a href="classroom.php?view=classroom&id=<?php echo $teacher['classroom']['id']; ?>">
                 <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                     <?php echo $teacher['classroom']['name']; ?></div>
                 </span>
+              </a>
               </td>
 
               <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
